56aa8051de5ce47523c2265e66e8e3be39dc8bde
Paul Grokenberger <grokepau@googlemail.com>
2020-02-17 19:01:05 +0200

    added listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :services, only: [:index, :show]
-
-  get "dashboard", to: 'pages#dashboard'
   root to: 'services#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/dashboard', to: 'pages#dashboard', as: :dashboard
