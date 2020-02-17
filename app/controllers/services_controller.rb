455c8d607a0cb844dec964d023d2208496b7e8b4
Paul Grokenberger <grokepau@googlemail.com>
2020-02-17 15:10:17 +0200

    added comments

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   def index
+    # gives everyone access to the index page logeed in or not !!
     @services = policy_scope(Service).order(created_at: :desc)
   end
 end
