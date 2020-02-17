c5b428b0470379a5ecd2d88c154fefd75673579d
Sean Keegan <sean@das.llc>
2020-02-17 16:47:27 +0200

    added authorization to services show

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,4 +3,10 @@ class ServicesController < ApplicationController
     # gives everyone access to the index page logeed in or not !!
     @services = policy_scope(Service).order(created_at: :desc)
   end
+
+  def show
+    @service = Service.find(params[:id])
+    authorize @service
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-resources :services, only: :index
+  resources :services, only: [:index, :show]
 
   get "dashboard", to: 'pages#dashboard'
   root to: 'services#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
