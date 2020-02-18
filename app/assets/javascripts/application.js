d7c48190798eacd2659b0c0503829483fccd650f
Paul Grokenberger <grokepau@googlemail.com>
2020-02-18 16:28:45 +0200

    added date and simpleform

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,3 @@
 //= require rails-ujs
 //= require_tree .
 
-//= require jquery
-//= require jquery_ujs
-//= require jquery.ui.all
-//= require turbolinks
-//= require_tree .
-
-$("input.datepicker").each(function(input) {
- $(this).datepicker({
-   dateFormat: "yy-mm-dd",
-   altField: $(this).next()
- })
