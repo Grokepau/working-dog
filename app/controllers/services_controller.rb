5233883fe0ffcb1ec6e30e2b95d322e4ef17bf0c
Paul Grokenberger <grokepau@googlemail.com>
2020-02-18 12:23:29 +0200

    added policy and methods for new/create

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   layout 'application-no-nav', only: :index
+  before_action :set_service, only: :show
   skip_before_action :authenticate_user!, only: :index
   def index
     # gives everyone access to the index page logeed in or not !!
@@ -7,8 +8,30 @@ class ServicesController < ApplicationController
   end
 
   def show
-    @service = Service.find(params[:id])
     authorize @service
   end
 
+  def new
+    @service = Service.new
+    authorize @service
+  end
+
+  def create
+    @service = Service.new(service_params)
+    authorize @service
+    if @service.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:title, :description, :user_id)
+  end
 end
