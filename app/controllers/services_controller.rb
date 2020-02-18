ffb2643350a9321eb6543f066bbf34fd68c9d97a
Paul Grokenberger <grokepau@googlemail.com>
2020-02-18 11:59:17 +0200

    added skip devise for index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   layout 'application-no-nav', only: :index
+  skip_before_action :authenticate_user!, only: :index
   def index
     # gives everyone access to the index page logeed in or not !!
     @services = policy_scope(Service).order(created_at: :desc)
