6da4667960a4020c854fa0ecb8782d82ea4acc0f
Sean Keegan <sean@das.llc>
2020-02-19 18:38:54 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,8 @@ Booking.destroy_all
 
 puts 'Constructing new users...'
 
-sonic_team = {'Lassie' => 'Dog',
+
+user_basics = {'Lassie' => 'Dog',
               'Flipper' => 'Dolphin',
               'Secretariat' => 'Horse',
               'Billy' => 'Goat',
@@ -23,13 +24,13 @@ sonic_team = {'Lassie' => 'Dog',
               'Big Bird' => 'Bird',
               'Willy' => 'Killer Whale'}
 
-photo_urls = 'Lassie' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582054739/mAetHQaMfgCEXfLbRNGBqN6p.png',
-             'Flipper' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582055384/kAgd2Ew9SscPhYeFAgeTy62W.png',
-             'Secretariat' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582109474/secretariat_zztoiv.png',
-             'Billy' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582109870/6-66555_transparent-goat-silhouette-png-goat-simulator-goat-png_pmko2a.png',
-             'Brownie' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582109293/LeWagon_portraits_2020.02.06.-53_1_kljass.png',
-             'Big Bird' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582110224/111-1110639_big-bird-face-png-sesame-street-big-bird_iyellv.png',
-             'Willy' => 'Killer Whale'}
+photo_urls ={ 'Lassie' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582054739/mAetHQaMfgCEXfLbRNGBqN6p.png',
+              'Flipper' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582055384/kAgd2Ew9SscPhYeFAgeTy62W.png',
+              'Secretariat' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582109474/secretariat_zztoiv.png',
+              'Billy' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582109870/6-66555_transparent-goat-silhouette-png-goat-simulator-goat-png_pmko2a.png',
+              'Brownie' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582109293/LeWagon_portraits_2020.02.06.-53_1_kljass.png',
+              'Big Bird' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582110224/111-1110639_big-bird-face-png-sesame-street-big-bird_iyellv.png',
+              'Willy' => 'https://res.cloudinary.com/djutcyvwz/image/upload/v1582111168/killer_whale_PNG14_obabd7.png'}
 
 
 locations = ["Ramat Gan",
@@ -38,68 +39,83 @@ locations = ["Ramat Gan",
              "Jersulem",
              "Herzilya"]
 
-sonic_team.each do |name, species|
-  new_user_details = {}
-  new_user_details[:username] = name
-  new_user_details[:species] = species
-  new_user_details[:password] = "#{name}_secret"
-  new_user_details[:location] = locations.sample
-  new_user_details[:email] = "#{name}@#{name}.com"
-  new_user_details[:admin] = false
-  User.create!(new_user_details)
+
+
+user_basics.each do |name, species|
+
+  # Construct new user, except for photo
+  details = {}
+  details[:username] = name
+  details[:species] = species
+  details[:password] = "#{name}_secret"
+  details[:location] = locations.sample
+  details[:email] = "#{name}@#{name}.com"
+  details[:admin] = false
+  new_user = User.new(details)
+
+  # Prepare photo from URL, attach to user
+  p "#{photo_urls[name]}"
+  p name
+
+
+  file = URI.open("#{photo_urls[name]}")
+  new_user.photo.attach(io: file, filename: "#{name}_avatar.png", content_type: 'image/png')
 end
 
-puts 'Done with normal users! Making an admin user -- data below...'
 puts 'Data for first user below...'
 p User.first
-User.create!( username: 'bossman',
+
+puts 'Done with normal users! Making an admin user -- data below...'
+User.create!( username: 'admin',
               species: 'human',
-              password: 'bossman_secret',
+              password: 'admin_secret',
               location: 'Israel',
-              email: 'bossman@human.com',
+              email: 'admin@admin.com',
               admin: 'true')
 puts 'Data for last user (admin) below...'
 p User.last
 puts ''
 puts '*** Note! password is "#{name}_secret ***"'
 puts ''
-puts 'Constructing Services'
 
-sonic_id = User.find_by(username: "sonic").id
-tails_id = User.find_by(username: "tails").id
-big_id = User.find_by(username: "big").id
-charmy_id = User.find_by(username: "charmy").id
 
-new_services = [Service.new(title: "Rescue Animals", description: "Gotta go fast!", user_id:  sonic_id)]
-new_services << Service.new(title: "Collect Rings", description: "Gotta be strong!", user_id:  sonic_id)
-new_services << Service.new(title: "Sidekick for a Day", description: "I'll catch you when you fall down a pit - I promise!", user_id: tails_id)
-new_services << Service.new(title: "Fishing", description: "Meoooooooooooow", user_id: charmy_id)
-new_services << Service.new(title: "Produce Honey", description: "Been doing this for yearzzzz!", user_id: charmy_id)
+# puts 'Constructing Services'
 
-puts 'Persisting Constructed Services...'
-new_services.each do |service|
-  p service
-  service.save!
-end
-puts ' ** Done seeding services! Data for first and last service below...'
-p Service.first
-p Service.last
+# sonic_id = User.find_by(username: "sonic").id
+# tails_id = User.find_by(username: "tails").id
+# big_id = User.find_by(username: "big").id
+# charmy_id = User.find_by(username: "charmy").id
 
-sidekick_id = Service.find_by(title: "Sidekick for a Day").id
-honey_id = Service.find_by(title: "Produce Honey").id
-fishing_id = Service.find_by(title: "Fishing").id
+# new_services = [Service.new(title: "Rescue Animals", description: "Gotta go fast!", user_id:  sonic_id)]
+# new_services << Service.new(title: "Collect Rings", description: "Gotta be strong!", user_id:  sonic_id)
+# new_services << Service.new(title: "Sidekick for a Day", description: "I'll catch you when you fall down a pit - I promise!", user_id: tails_id)
+# new_services << Service.new(title: "Fishing", description: "Meoooooooooooow", user_id: charmy_id)
+# new_services << Service.new(title: "Produce Honey", description: "Been doing this for yearzzzz!", user_id: charmy_id)
 
-puts ' * Constructing sample bookings... * '
+# puts 'Persisting Constructed Services...'
+# new_services.each do |service|
+#   p service
+#   service.save!
+# end
+# puts ' ** Done seeding services! Data for first and last service below...'
+# p Service.first
+# p Service.last
 
-today = Date.today
-tomorrow = Date.today + 1
-day_after_tomorrow = Date.today + 2
-Booking.create(user_id: sonic_id, service_id: sidekick_id, date: today)
-Booking.create(user_id: sonic_id, service_id: sidekick_id, date: tomorrow)
-Booking.create(user_id: sonic_id, service_id: honey_id, date: day_after_tomorrow)
-Booking.create(user_id: big_id, service_id: sidekick_id, date: day_after_tomorrow)
+# sidekick_id = Service.find_by(title: "Sidekick for a Day").id
+# honey_id = Service.find_by(title: "Produce Honey").id
+# fishing_id = Service.find_by(title: "Fishing").id
 
-puts " ***  Done with seeding! *** "
+# puts ' * Constructing sample bookings... * '
+
+# today = Date.today
+# tomorrow = Date.today + 1
+# day_after_tomorrow = Date.today + 2
+# Booking.create(user_id: sonic_id, service_id: sidekick_id, date: today)
+# Booking.create(user_id: sonic_id, service_id: sidekick_id, date: tomorrow)
+# Booking.create(user_id: sonic_id, service_id: honey_id, date: day_after_tomorrow)
+# Booking.create(user_id: big_id, service_id: sidekick_id, date: day_after_tomorrow)
+
+# puts " ***  Done with seeding! *** "
 
 
 
